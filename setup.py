eb630a5e9c188ba8a7f69020d027bb222a13e8fb
I <ibelyalov@yandex.ru>
2019-02-24 13:38:10 +0300

    I should probably pay more attention

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=[
         "ufal.udpipe==1.2.0.1",
         "pymystem3==0.2.0",
-        "russian-tagsets @ https://github.com/theotheo/russian-tagsets@master"
+        "russian-tagsets @ git+https://github.com/theotheo/russian-tagsets@master"
     ],
     scripts=['bin/udpiper']
 )
